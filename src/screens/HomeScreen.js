973e544dbb60eb4ca0a315bb78bd9e786f34bc26
Alejandro F <afuste7@gmail.com>
2024-03-04 15:30:14 -0500

    added fonts for instructions text

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,8 +9,15 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
+      <View style={[styles.headerContainer, styles.paddingBottom]}>
         <Logo width="100%" height="100%" />
+      </View>
+
+      <View>
+        <Text style={[styles.leagueFont, styles.paddingBottom]}>Leagues</Text>
+        <Text style={[styles.instructionFont, styles.paddingBottom]}>
+          Click on a league to continue
+        </Text>
       </View>
 
       <View style={styles.contentContainer}>
@@ -30,22 +37,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#122d42",
+    padding: 10,
   },
   headerContainer: {
     flex: 1,
     position: "relative",
-    left: -50,
-    paddingRight: 10,
-    paddingBottom: 10,
-    paddingLeft: 10,
+    top: -10,
+    left: -65,
   },
   logo: {
     width: 66,
     height: 58,
   },
+  leagueFont: {
+    fontFamily: "RobotoCondensed-Medium",
+    fontSize: 20,
+    fontWeight: "500",
+    color: "white",
+  },
+  instructionFont: {
+    fontFamily: "RobotoCondensed-Medium",
+    fontSize: 14,
+    color: "white",
+  },
   contentContainer: {
     flex: 10,
-    padding: 10,
     alignItems: "center",
+    justifyContent: "center",
+  },
+  paddingBottom: {
+    paddingBottom: 10,
   },
 });
