11357e86bedd10dae84d3e6794319b710b93c3d4
Alejandro F <afuste7@gmail.com>
2024-03-09 22:21:17 -0500

    changed styling to overall info card in player model

--- src/components/PlayerContent.js
+++ src/components/PlayerContent.js
@@ -21,35 +21,75 @@ const PlayerContent = ({ data }) => {
           <Image src={headShot} style={styles.headShot} />
 
           <View style={styles.subHeader2Container}>
-            <Text style={styles.playerInfo}>{name}</Text>
-            <Text style={styles.playerInfo}>{team}</Text>
-            <Text style={styles.playerInfo}>{position}</Text>
+            <Text style={styles.playerInfoTitle}>{name}</Text>
+            <Text style={styles.playerInfoFont}>{team}</Text>
+            <Text style={styles.playerInfoFont}>{position}</Text>
           </View>
 
           <View testID="overGradeView">
-            <Text style={styles.playerInfo}>Grade</Text>
-            <Text style={styles.playerInfo}>{overallGrade.value}</Text>
-            <Text style={styles.playerInfo}>{overallGrade.grade.letter}</Text>
+            <Text style={styles.playerInfoTitle}>Grade</Text>
+            <Text style={styles.playerInfoFont}>{overallGrade.value}</Text>
+            <Text style={styles.playerInfoFont}>
+              {overallGrade.grade.letter}
+            </Text>
           </View>
         </View>
 
         <View style={styles.contentContainer}>
-          <Text>
-            PlayerScreen - Display player info...use Flatlist to dipslay all
-            info
-          </Text>
-          <View>
-            <Text>Overall Info</Text>
-            <Text>
-              Grade: {overallGrade.value} {overallGrade.grade.letter}
+          {/* Overall Info */}
+          <View
+            testID="overallInfoView"
+            style={[styles.infoContainer, styles.columnContainer]}
+          >
+            <Text style={[styles.titleFont, styles.statsColor]}>
+              Overall Info
             </Text>
-            <Text>Descripton: {overallGrade.grade.description}</Text>
-            <Text>Overall Rank: {production.overallRank}</Text>
-            <Text>Overall Percentile: {production.overallPercentile}%</Text>
+            <View style={styles.rowContainer}>
+              <Text
+                style={[styles.statsColor, styles.statsTitleFont]}
+                testID="descriptionTitle"
+              >
+                Descripton:{" "}
+              </Text>
+              <Text style={styles.valueColor} testID="descriptionValue">
+                {overallGrade.grade.description} Player
+              </Text>
+            </View>
+
+            <View style={styles.rowContainer}>
+              <Text
+                style={[styles.statsColor, styles.statsTitleFont]}
+                testID="descriptionTitle"
+              >
+                Overall Rank:{" "}
+              </Text>
+              <Text style={styles.valueColor} testID="overallRankValue">
+                {production.overallRank}
+              </Text>
+            </View>
+
+            <View style={styles.rowContainer}>
+              <Text
+                style={[styles.statsColor, styles.statsTitleFont]}
+                testID="overallPercentileTile"
+              >
+                Overall Percentile:
+              </Text>
+              <Text style={styles.valueColor} testID="overallPercentileValue">
+                {production.overallPercentile}%
+              </Text>
+            </View>
           </View>
 
-          <View>
-            <Text>Offense Info</Text>
+          {/* Offense Info */}
+
+          <View
+            testID="offenseInfoView"
+            style={[styles.infoContainer, styles.columnContainer]}
+          >
+            <Text style={[styles.titleFont, styles.statsColor]}>
+              Offense Info
+            </Text>
             <Text>Points Per Game: {offense.pointsPerGame}</Text>
             <Text>Rank: {offense.rank}</Text>
             <Text>Percentile: {offense.percentile}%</Text>
@@ -61,8 +101,15 @@ const PlayerContent = ({ data }) => {
             <Text>Total Yards Per Game: {offense.yardsPerGame}</Text>
           </View>
 
-          <View>
-            <Text>Production Info</Text>
+          {/* Production Info */}
+
+          <View
+            testID="productionInfoView"
+            style={[styles.infoContainer, styles.columnContainer]}
+          >
+            <Text style={[styles.titleFont, styles.statsColor]}>
+              Production Info
+            </Text>
             <Text>Total Points: {production.totalPoints}</Text>
             <Text>Points Per Game: {production.pointsPerGame}</Text>
             <Text>Position Rank: {production.positionRank}</Text>
@@ -73,8 +120,15 @@ const PlayerContent = ({ data }) => {
             </Text>
           </View>
 
-          <View>
-            <Text>Opportunity Info</Text>
+          {/* Opportunity Info */}
+
+          <View
+            testID="opportunityInfoView"
+            style={[styles.infoContainer, styles.columnContainer]}
+          >
+            <Text style={[styles.titleFont, styles.statsColor]}>
+              Opportunity Info
+            </Text>
             {determinePositionOpportunity(position, opportunity)}
           </View>
         </View>
@@ -109,22 +163,51 @@ const styles = StyleSheet.create({
     flex: 0,
     padding: 10,
   },
-  playerInfo: {
+  playerInfoTitle: {
     color: "white",
     fontSize: 18,
     fontWeight: "bold",
   },
+  playerInfoFont: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: 500,
+  },
   contentContainer: {
     flex: 10,
-    padding: 10,
-    backgroundColor: "white",
-    alignItems: "center",
+    padding: 5,
+    backgroundColor: "#f2f2f2",
     justifyContent: "space-evenly",
+  },
+  columnContainer: {
+    flex: 0,
+    flexDirection: "column",
+  },
+  rowContainer: {
+    flex: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  infoContainer: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
   },
   headShot: {
     width: 100,
     height: 100,
     resizeMode: "cover",
     borderRadius: 50,
+  },
+  titleFont: {
+    color: "#454545",
+    fontSize: 15,
+    fontWeight: 700,
+    marginBottom: 5,
+  },
+  valueColor: {
+    color: "#5c5c5c",
+    marginBottom: 5,
   },
 });
